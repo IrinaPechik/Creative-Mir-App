8aebbe9b02aa323b5503ce657ae7d3c83242fb12
irinapechik <irinapechik@gmail.com>
2024-01-31 14:31:03 +0300

    added BirthdateEnteringView

---
+++ Creative-Mir/View/SignUp/BirthdateEnteringView.swift
@@ -0,0 +1,18 @@
+//
+//  BirthdateEnteringView.swift
+//  Creative-Mir
+//
+//  Created by Печик Дарья on 31.01.2024.
+//
+
+import SwiftUI
+
+struct BirthdateEnteringView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BirthdateEnteringView()
+}
