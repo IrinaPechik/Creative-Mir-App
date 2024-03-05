c974342ab514a140b66021ac7b58974423577941
irinapechik <irinapechik@gmail.com>
2024-03-05 19:12:09 +0300

    changed supplier model

--- Creative-Mir/Model/MWSupplier.swift
+++ Creative-Mir/Model/MWSupplier.swift
@@ -7,9 +7,29 @@
 
 import Foundation
 
-struct MWSupplier: Identifiable {
+struct MWSupplier: Identifiable, Codable {
     var id: String
     var storyAboutYourself: String
+    var advertisements: [SupplierAdvertisemnt] = []
+    
+    var representation: [String: Any] {
+        var repres = [String: Any]()
+        repres["id"] = self.id
+        repres["storyAboutYourself"] = self.storyAboutYourself
+        repres["advertisements"] = self.advertisements
+        // Преобразование каждого advertisement в его словарное представление
+        let adsRepresentation = advertisements.map { $0.representation }
+        repres["advertisements"] = adsRepresentation
+
+        return repres
+    }
+    
+    mutating func addAdvertisement(advertisement: SupplierAdvertisemnt) {
+        advertisements.append(advertisement)
+    }
+}
+
+struct SupplierAdvertisemnt: Codable {
     var legalStatus: String
     var stageName: String?
     var companyName: String?
@@ -22,16 +42,15 @@ struct MWSupplier: Identifiable {
     
     var representation: [String: Any] {
         var repres = [String: Any]()
-        repres["storyAboutYourself"] = self.storyAboutYourself
-        repres["legalStatus"] = self.legalStatus
-        repres["stageName"] = self.stageName
-        repres["companyName"] = self.companyName
-        repres["companyPosition"] = self.companyPosition
-        repres["skill"] = self.skill
-        repres["experience"] = self.experience
-        repres["experienceMeasure"] = self.experienceMeasure
-        repres["storyAboutWork"] = self.storyAboutWork
-        repres["photosFromWork"] = self.photosFromWork
+        repres["legalStatus"] = legalStatus
+        repres["stageName"] = stageName
+        repres["companyName"] = companyName
+        repres["companyPosition"] = companyPosition
+        repres["skill"] = skill
+        repres["experience"] = experience
+        repres["experienceMeasure"] = experienceMeasure
+        repres["storyAboutWork"] = storyAboutWork
+        repres["photosFromWork"] = photosFromWork
         return repres
     }
 }
