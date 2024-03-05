b3cf2e93189879157b3870683015e6fe3feb6bd4
irinapechik <irinapechik@gmail.com>
2024-03-05 19:13:37 +0300

    db service changed

--- Creative-Mir/Services/DatabaseService.swift
+++ Creative-Mir/Services/DatabaseService.swift
@@ -16,6 +16,18 @@ class DatabaseService {
         return db.collection("users")
     }
     
+    private var suppliersRef: CollectionReference {
+        return db.collection("suppliers")
+    }
+    
+    private var venuesRef: CollectionReference {
+        return db.collection("venues")
+    }
+    
+    private var customersRef: CollectionReference {
+        return db.collection("customers")
+    }
+    
     private init() {}
 
     func setUser(user: MWUser, completion: @escaping (Result<MWUser, Error>) -> ()) {
@@ -29,11 +41,54 @@ class DatabaseService {
     }
     
     func setSupplier(supplier: MWSupplier, completion: @escaping (Result<MWSupplier, Error>) -> ()) {
-        usersRef.document(supplier.id).setData(supplier.representation) { error in
+        suppliersRef.document(supplier.id).setData(supplier.representation) { error in
             if let error = error {
                 completion(.failure(error))
             } else {
                 completion(.success(supplier))
+            }
+        }
+    }
+    
+    func setVenue(venue: MWVenue, completion: @escaping (Result<MWVenue, Error>) -> ()) {
+        venuesRef.document(venue.id).setData(venue.representation) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(venue))
+            }
+        }
+    }
+    
+    func setCustomer(customer: MWCustomer, completion: @escaping (Result<MWCustomer, Error>) -> ()) {
+        customersRef.document(customer.id).setData(customer.representation) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(customer))
+            }
+        }
+    }
+    
+    func checkUserExist(email: String, completion: @escaping (Bool) -> Void) {
+        usersRef.whereField("email", isEqualTo: email).getDocuments { querySnapshot, error in
+            if let error = error {
+                print("Error fetching user document: \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+            // Проверяем, есть ли возвращенные документы
+            if let querySnapshot = querySnapshot {
+                if querySnapshot.isEmpty {
+                    // Документов с указанным email не найдено
+                    completion(false)
+                } else {
+                    // Найден один или более документов с указанным email
+                    completion(true)
+                }
+            } else {
+                // Запрос вернул nil
+                completion(false)
             }
         }
     }
