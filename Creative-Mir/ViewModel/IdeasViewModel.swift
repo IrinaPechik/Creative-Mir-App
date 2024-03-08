2ddd82fcc054bbb5362d83d94541c1e9df8ea513
irinapechik <irinapechik@gmail.com>
2024-03-09 00:55:12 +0300

    added view models

---
+++ Creative-Mir/ViewModel/IdeasViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  IdeasViewModel.swift
+//  Creative-Mir
+//
+//  Created by Печик Ирина on 08.03.2024.
+//
+
+import Foundation
+
+class IdeasViewModel: ObservableObject {
+    @Published var ideas: [MWIdea] = [MWIdea]()
+
+    func getIdeas() {
+        DatabaseService.shared.getIdeas { result in
+            switch result {
+            case .success(let ideas):
+                self.ideas = ideas
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
