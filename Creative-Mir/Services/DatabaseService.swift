764fbdc008e37166e01f3590606df9b86f0064f0
irinapechik <irinapechik@gmail.com>
2024-03-16 17:42:32 +0300

    added methods to communicate with db

--- Creative-Mir/Services/DatabaseService.swift
+++ Creative-Mir/Services/DatabaseService.swift
@@ -37,6 +37,8 @@ class DatabaseService {
     }
     
     private init() {}
+    
+    // MARK: -  Setting all types of users to data base.
 
     func setUser(user: MWUser, imageData: Data, completion: @escaping (Result<MWUser, Error>) -> ()) {
         usersRef.document(user.id).setData(user.representation) { error in
@@ -102,7 +104,7 @@ class DatabaseService {
             }
         }
     }
-    
+    // MARK: - User settings
     func checkUserExist(email: String, completion: @escaping (Bool) -> Void) {
         usersRef.whereField("email", isEqualTo: email).getDocuments { querySnapshot, error in
             if let error = error {
@@ -126,6 +128,290 @@ class DatabaseService {
         }
     }
     
+    func getUserRole(email: String, completion: @escaping (Result<String, Error>) -> ()) {
+        usersRef.getDocuments { qSnap, error in
+            if let qSnap = qSnap {
+                var user: MWUser
+                for doc in qSnap.documents {
+                    if let user1 = MWUser(doc: doc), user1.email == email {
+                        user = user1
+                        completion(.success(user.role))
+                    }
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    // MARK: -  Getting all types of users to data base.
+    
+    func getUser(id: String, completion: @escaping (Result<MWUser, Error>) -> ()) {
+        usersRef.getDocuments { qSnap, error in
+            if let qSnap = qSnap {
+                var user: MWUser
+                for doc in qSnap.documents {
+                    if let user1 = MWUser(doc: doc), user1.id == id {
+                        user = user1
+                        completion(.success(user))
+                    }
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func getSuppliers(completion: @escaping (Result<[MWSupplier], Error>) -> ()) {
+        suppliersRef.getDocuments { qSnap, error in
+            if let qSnap = qSnap {
+                var suppliers = [MWSupplier]()
+                for doc in qSnap.documents {
+                    if let supplier = MWSupplier(doc: doc) {
+                        suppliers.append(supplier)
+                    }
+                }
+                completion(.success(suppliers))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func getVenues(completion: @escaping (Result<[MWVenue], Error>) -> ()) {
+        venuesRef.getDocuments { qSnap, error in
+            if let qSnap = qSnap {
+                var venues = [MWVenue]()
+                for doc in qSnap.documents {
+                    if let venue = MWVenue(doc: doc) {
+                        venues.append(venue)
+                    }
+                }
+                completion(.success(venues))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    // MARK: - Info about advertisements likes
+    
+
+    // MARK:  Метод добавления поставщика услуг в избранное покупателя
+    func setLikedSupplierToCustomer(customerId: String, supplier: MWSupplier, completion: @escaping (Error?) -> Void) {
+        let likedSuppliersRef = customersRef.document(customerId).collection("likedSuppliers")
+        
+        likedSuppliersRef.addDocument(data: supplier.representation) { error in
+            if let error = error {
+                print("Error adding product to favorites: \(error.localizedDescription)")
+                completion(error)
+            } else {
+                print("Product added to favorites successfully")
+                completion(nil)
+            }
+        }
+    }
+    
+    // MARK:  Метод добавления арендодателя в избранное покупателя
+    func setLikedVenueToCustomer(customerId: String, venue: MWVenue, completion: @escaping (Error?) -> Void) {
+        let likedVenuesRef = customersRef.document(customerId).collection("likedVenues")
+        
+        likedVenuesRef.addDocument(data: venue.representation) { error in
+            if let error = error {
+                print("Error adding product to favorites: \(error.localizedDescription)")
+                completion(error)
+            } else {
+                print("Product added to favorites successfully")
+                completion(nil)
+            }
+        }
+    }
+    
+    // MARK: Метод удаления поставщика услуг из избранного покупателя
+    func deleteLikedSupplierToCustomer(customerId: String, supplier: MWSupplier, completion: @escaping (Error?) -> Void) {
+        let likedSuppliersRef = customersRef.document(customerId).collection("likedSuppliers")
+        let query = likedSuppliersRef.whereField("id", isEqualTo: supplier.id)
+        
+        query.getDocuments { snapshot, error in
+            if let error = error {
+                print("Error removing product from favorites: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            guard let documents = snapshot?.documents else {
+                print("Document not found")
+                completion(nil)
+                return
+            }
+            
+            for document in documents {
+                document.reference.delete { error in
+                    if let error = error {
+                        print("Error removing product from favorites: \(error.localizedDescription)")
+                        completion(error)
+                    } else {
+                        print("Product removed from favorites successfully")
+                        completion(nil)
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: Метод удаления арендодателя из избранного покупателя
+    func deleteLikedVenueToCustomer(customerId: String, venue: MWVenue, completion: @escaping (Error?) -> Void) {
+        let likedVenuesRef = customersRef.document(customerId).collection("likedVenues")
+        let query = likedVenuesRef.whereField("id", isEqualTo: venue.id)
+        
+        query.getDocuments { snapshot, error in
+            if let error = error {
+                print("Error removing product from favorites: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            guard let documents = snapshot?.documents else {
+                print("Document not found")
+                completion(nil)
+                return
+            }
+            
+            for document in documents {
+                document.reference.delete { error in
+                    if let error = error {
+                        print("Error removing product from favorites: \(error.localizedDescription)")
+                        completion(error)
+                    } else {
+                        print("Product removed from favorites successfully")
+                        completion(nil)
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: Метод получения избранных поставщиков услуг
+    func getLikedSuppliers(customerId: String, completion: @escaping (Result<[MWSupplier], Error>) -> ()) {
+        let likedSuppliersRef = customersRef.document(customerId).collection("likedSuppliers")
+
+        likedSuppliersRef.getDocuments { snapshot, error in
+            if let error = error {
+                            print("Error fetching favorite products: \(error.localizedDescription)")
+                            completion(.failure(error))
+                            return
+            }
+                        
+            guard let documents = snapshot?.documents else {
+                print("No favorite products found")
+                completion(.success([]))
+                return
+            }
+//            let likedSuppliers = documents.compactMap {document -> MWSupplier? in
+//                guard let supplierId = document.data()["id"] as? String else {
+//                    print("supplier not found")
+//                    return nil
+//                }
+//                return MWSupplier(id: supplierId)
+//            }
+//            completion(.success(likedSuppliers))
+            let likedSuppliers = documents.compactMap { document -> MWSupplier? in
+                        let data = document.data()
+                        guard let id = data["id"] as? String,
+                              let storyAboutYourself = data["storyAboutYourself"] as? String,
+                              let advertisementsData = data["advertisements"] as? [[String: Any]] else {
+                            print("Invalid supplier data")
+                            return nil
+                        }
+                        var advertisements: [SupplierAdvertisemnt] = []
+                        for advData in advertisementsData {
+                            let adv = SupplierAdvertisemnt(from: advData)
+                            advertisements.append(adv)
+                        }
+                        return MWSupplier(id: id, storyAboutYourself: storyAboutYourself, advertisements: advertisements)
+                    }
+                    completion(.success(likedSuppliers))
+        }
+    }
+    
+    // MARK: Метод получения избранных арендодателей
+    func getLikedVenues(customerId: String, completion: @escaping (Result<[MWVenue], Error>) -> ()) {
+        let likedVenuesRef = customersRef.document(customerId).collection("likedVenues")
+
+        likedVenuesRef.getDocuments { snapshot, error in
+            if let error = error {
+                            print("Error fetching favorite products: \(error.localizedDescription)")
+                            completion(.failure(error))
+                            return
+            }
+                        
+            guard let documents = snapshot?.documents else {
+                print("No favorite products found")
+                completion(.success([]))
+                return
+            }
+            let likedVenues = documents.compactMap { document -> MWVenue? in
+                        let data = document.data()
+                        guard let id = data["id"] as? String,
+                              let advertisementsData = data["advertisements"] as? [[String: Any]] else {
+                            print("Invalid supplier data")
+                            return nil
+                        }
+                        var advertisements: [VenueAdvertisemnt] = []
+                        for advData in advertisementsData {
+                            let adv = VenueAdvertisemnt(from: advData)
+                            advertisements.append(adv)
+                        }
+                        return MWVenue(id: id, advertisements: advertisements)
+                    }
+                    completion(.success(likedVenues))
+        }
+    }
+    
+    // MARK: Метод проверки есть ли поставщик услуг в избранном у покупателя
+    func checkWasSupplierLikedById(customerId: String, supplierId: String, completion: @escaping (Result<Bool, Error>) -> ()) {
+        let likedSuppliersRef = customersRef.document(customerId).collection("likedSuppliers")
+
+        let query = likedSuppliersRef.whereField("id", isEqualTo: supplierId)
+        query.getDocuments { snapshot, error in
+            if let error = error {
+                print("Error checking if supplier is liked by id: \(error.localizedDescription)")
+                completion(.failure(error))
+                return
+            }
+            
+            guard let documents = snapshot?.documents else {
+                print("No favorite products found for id: \(supplierId)")
+                completion(.success(false))
+                return
+            }
+            
+            // Если количество документов больше нуля, значит продукт найден в списке избранных
+            let isLiked = documents.count > 0
+            completion(.success(isLiked))
+        }
+    }
+    
+    // MARK: Метод проверки есть ли арендодатель в избранном у покупателя
+    func checkWasVenueLikedById(customerId: String, venueId: String, completion: @escaping (Result<Bool, Error>) -> ()) {
+        let likedVenuesRef = customersRef.document(customerId).collection("likedVenues")
+
+        let query = likedVenuesRef.whereField("id", isEqualTo: venueId)
+        query.getDocuments { snapshot, error in
+            if let error = error {
+                print("Error checking if supplier is liked by id: \(error.localizedDescription)")
+                completion(.failure(error))
+                return
+            }
+            
+            guard let documents = snapshot?.documents else {
+                print("No favorite products found for id: \(venueId)")
+                completion(.success(false))
+                return
+            }
+            
+            // Если количество документов больше нуля, значит продукт найден в списке избранных
+            let isLiked = documents.count > 0
+            completion(.success(isLiked))
+        }
+    }
+    // MARK: - Getting and setting ideas and categories
     func getIdeas(by categoryId: String, completion: @escaping (Result<[MWIdea], Error>) -> ()) {
         ideasRef.getDocuments { qSnap, error in
             if let qSnap = qSnap {
@@ -177,37 +463,4 @@ class DatabaseService {
             }
         }
     }
-    
-    func getUserRole(email: String, completion: @escaping (Result<String, Error>) -> ()) {
-        usersRef.getDocuments { qSnap, error in
-            if let qSnap = qSnap {
-                var user: MWUser
-                for doc in qSnap.documents {
-                    if let user1 = MWUser(doc: doc), user1.email == email {
-                        user = user1
-                        completion(.success(user.role))
-                    }
-                }
-            } else if let error = error {
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    func getUser(id: String, completion: @escaping (Result<MWUser, Error>) -> ()) {
-        usersRef.getDocuments { qSnap, error in
-            if let qSnap = qSnap {
-                var user: MWUser
-                for doc in qSnap.documents {
-                    if let user1 = MWUser(doc: doc), user1.id == id {
-                        user = user1
-                        completion(.success(user))
-                    }
-                }
-            } else if let error = error {
-                completion(.failure(error))
-            }
-        }
-    }
-    
 }
