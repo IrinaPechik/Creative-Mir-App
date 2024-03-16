4dfedd0032c9e6aa85ca950655ef79f10be8cd8c
irinapechik <irinapechik@gmail.com>
2024-03-16 22:19:13 +0300

    added supplierCard

---
+++ Creative-Mir/View/Customers'View/SuppliersInfoViews/SupplierCard.swift
@@ -0,0 +1,18 @@
+//
+//  SupplierCard.swift
+//  Creative-Mir
+//
+//  Created by Печик Дарья on 16.03.2024.
+//
+
+import SwiftUI
+
+struct SupplierCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SupplierCard()
+}
