3e72b2df6dde1d9562bc2d75a14c2bcfb62e70f3
irinapechik <irinapechik@gmail.com>
2024-03-31 15:12:41 +0300

    added supplier card view

--- Creative-Mir/View/Customers'View/SuppliersInfoViews/SupplierCard.swift
+++ Creative-Mir/View/Customers'View/SuppliersInfoViews/SupplierCard.swift
@@ -11,35 +11,93 @@ struct SupplierCard: View {
     @Binding var supplier: MWSupplier
     @Binding var user: MWUser
     @State var uiImage: UIImage? = nil
+    @State var photosFromWork: [UIImage] = []
     @Binding var advIndex: Int
     var body: some View {
         VStack {
             if let uiImage = uiImage {
-                HStack {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .frame(width: 138, height: 138)
-                        .aspectRatio(contentMode: .fit)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                    VStack(alignment: .leading) {
-                        if supplier.advertisements[advIndex].legalStatus == "individual" {
-                            if supplier.advertisements[advIndex].stageName == nil {
-                                Text("\(user.name) \(user.surname)")
-                            } else {
-                                Text("\(supplier.advertisements[advIndex].stageName!)")
+                Spacer()
+                VStack {
+                    VStack(alignment: .leading, spacing: 3) {
+                        Text(supplier.advertisements[advIndex].skill)
+                            .font(customFont: .PlayfairDisplayMedium, size: 48)
+                        HStack {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .frame(width: 138, height: 138)
+                                .aspectRatio(contentMode: .fit)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                            VStack(alignment: .leading) {
+                                if supplier.advertisements[advIndex].legalStatus == "individual" {
+                                    if supplier.advertisements[advIndex].stageName == nil {
+                                        Text("\(user.name) \(user.surname)")
+                                            .font(customFont: .PlayfairDisplayMedium, size: 30)
+                                    } else {
+                                        Text("\(supplier.advertisements[advIndex].stageName!)")
+                                    }
+                                } else {
+                                    Text("\(supplier.advertisements[advIndex].companyName!), \(supplier.advertisements[advIndex].companyPosition!)")
+                                        .font(customFont: .PlayfairDisplayMedium, size: 30)
+                                    
+                                }
+                                Text(user.residentialAddress)
+                                    .font(customFont: .LoraRegular, size: 18)
+                                Text("\(supplier.advertisements[advIndex].experience) \(supplier.advertisements[advIndex].experienceMeasure) of experience")
+                                    .font(customFont: .LoraRegular, size: 18)
+                                Text(user.email)
+                                    .font(customFont: .LoraRegular, size: 18)
                             }
-                        } else {
-                            Text("\(supplier.advertisements[advIndex].companyName!), \(supplier.advertisements[advIndex].companyPosition!)")
+                            Spacer()
                         }
-                        Text(user.residentialAddress)
-                        Text("\(supplier.advertisements[advIndex].experience) \(supplier.advertisements[advIndex].experienceMeasure) of experience")
-                        Text(user.email)
                     }
-                    
+                    .padding(.leading)
+                    Spacer()
+                    ScrollView {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("About me:").font(customFont: .LoraRegular, size: 24)
+                                    .padding(.trailing)
+                                Text(supplier.storyAboutYourself)
+                                    .font(customFont: .OpenSansLight, size: 16)
+                                Spacer()
+                            }
+                            .padding()
+                            HStack {
+                                Text("About my\n career:").font(customFont: .LoraRegular, size: 24)
+                                    .padding(.trailing)
+                                Text(supplier.storyAboutYourself)
+                                    .font(customFont: .OpenSansLight, size: 16)
+                                Spacer()
+                            }
+                            .padding()
+                            
+                            ScrollView {
+                                HStack {
+                                    Text("My work\n photos:").font(customFont: .LoraRegular, size: 24)
+                                        .padding(.trailing)
+                                    ForEach(photosFromWork, id: \.self) { photo in
+                                        Image(uiImage: photo)
+                                            .resizable()
+                                            .frame(width: 110, height: 110)
+                                            .aspectRatio(contentMode: .fit)
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .ignoresSafeArea(edges: .bottom)
+
                 }
-//                Text(supplier.storyAboutYourself)
             }
         }
+        .frame(maxWidth: .infinity)
+        .background(Color.backgroundColor)
+
         .onAppear {
             StorageService.shared.downloadUserAvatarImage(id: supplier.id) { result in
                 switch result {
@@ -51,10 +109,23 @@ struct SupplierCard: View {
                     print(error.localizedDescription)
                 }
             }
+            
+            StorageService.shared.downloadSuppliersPhotoFromWorkImages(id: supplier.id) { result in
+                switch result {
+                case .success(let data):
+                    for photo in data {
+                        if let img = UIImage(data: photo) {
+                            photosFromWork.append(img)
+                        }
+                    }
+                case.failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
 
 #Preview {
-    SupplierCard(supplier: .constant(MWSupplier(id: "BU7G1DeIskbftR05qMc5jL0uIZf2", storyAboutYourself: "Music isn't just my job; it's my lifeblood. From the moment the music starts playing, I feel alive, and I want nothing more than to share that feeling with everyone around me. My sets are more than just a collection of songs; they're stories waiting to be told, emotions waiting to be felt. When I see the crowd moving and feeling the music as deeply as I do, it's like pure magic. That's what being a DJ means to me - spreading joy, energy, and love through the power of music.", advertisements: [SupplierAdvertisemnt(legalStatus: "individual", skill: "DJ", experience: 3, experienceMeasure: "years", storyAboutWork: "I'm Anna, a DJ bursting with passion and energy. When I step behind the decks, it's like stepping into a world where music becomes magic. With every beat I drop, I aim to take my audience on a thrilling journey filled with rhythm and emotion.")])), user: .constant(MWUser(id: "BU7G1DeIskbftR05qMc5jL0uIZf2", email: "anna@gmail.com", name: "Anne", surname: "Daniel", birthday: "31.12.1998", residentialAddress: "Cotati, United States", role: "supplier")), advIndex: .constant(0))
+    SupplierCard(supplier: .constant(MWSupplier(id: "BU7G1DeIskbftR05qMc5jL0uIZf2", storyAboutYourself: "Music isn't just my job; it's my lifeblood. From the moment the music starts playing, I feel alive, and I want nothing more than to share that feeling with everyone around me. My sets are more than just a collection of songs; they're stories waiting to be told, emotions waiting to be felt. When I see the crowd moving and feeling the music as deeply as I do, it's like pure magic. That's what being a DJ means to me - spreading joy, energy, and love through the power of music.", advertisements: [SupplierAdvertisemnt(legalStatus: "individual", skill: "DJ", experience: 3, experienceMeasure: "years", storyAboutWork: "I'm Anna, a DJ bursting with passion and energy. When I step behind the decks, it's like stepping into a world where music becomes magic. With every beat I drop, I aim to take my audience on a thrilling journey filled with rhythm and emotion.")])), user: .constant(MWUser(id: "BU7G1DeIskbftR05qMc5jL0uIZf2", email: "anna@gmail.com", name: "Anne", surname: "Daniel", birthday: "31.12.1998", residentialAddress: "Cotati, United States", role: "supplier")), photosFromWork: [], advIndex: .constant(0))
 }
